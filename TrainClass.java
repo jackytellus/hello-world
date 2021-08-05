2cdbed0e8c954dfe40ee723b5fa5b3b1f20a49eb
jackytellus <88465464+jackytellus@users.noreply.github.com>
2021-08-04 18:41:40 -0700

    Added properties
    Added 2 properties: trainType, trainName

--- TrainClass.java
+++ TrainClass.java
@@ -1,3 +1,25 @@
 public class TrainClass
 {
+    protected int trainType = null;
+    protected String trainName = null;
+
+    private TrainClass()
+    {
+    }
+
+    public TrainClass(int theTrainType, String theTrainName)
+    {
+        this.trainType = theTrainType;
+        this.trainName = theTrainName;
+    }
+
+    public int getTrainType()
+    {
+        return this.trainType;
+    }
+
+    public String getTrainName()
+    {
+        return this.trainName;
+    }
 }
